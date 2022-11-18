a9c2570d2a86bd555ae5bc893a81d4ebecd3a964
Prakash Banjade <prakashbanjade191@gmail.com>
2022-11-18 22:48:16 +0545

    Updates

---
+++ precache-manifest.e248716f6f8b32ee7aebd2fbc19dc0ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4c950700637e777bd0c",
+    "url": "/dictionary/static/css/main.662c220d.chunk.css"
+  },
+  {
+    "revision": "f4c950700637e777bd0c",
+    "url": "/dictionary/static/js/main.f4c95070.chunk.js"
+  },
+  {
+    "revision": "8064b06839c8f860fbfa",
+    "url": "/dictionary/static/js/1.8064b068.chunk.js"
+  },
+  {
+    "revision": "be32c7bf30fc8e497c25",
+    "url": "/dictionary/static/js/runtime~main.be32c7bf.js"
+  },
+  {
+    "revision": "04dcb6974b39ac3ab9ac9afe66c4efba",
+    "url": "/dictionary/index.html"
+  }
+];
