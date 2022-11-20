18d3bea102bf81ab9ec5e741e1030e13785e6c77
Prakash Banjade <prakashbanjade191@gmail.com>
2022-11-20 22:11:34 +0545

    Updates

---
+++ precache-manifest.e8625aa25acfa0a8b95cb8c96b10804e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd80b894edaa5bab365a",
+    "url": "/dictionary/static/css/main.e6ee5346.chunk.css"
+  },
+  {
+    "revision": "fd80b894edaa5bab365a",
+    "url": "/dictionary/static/js/main.fd80b894.chunk.js"
+  },
+  {
+    "revision": "f6fcdcc9d4e7dd439cc1",
+    "url": "/dictionary/static/js/1.f6fcdcc9.chunk.js"
+  },
+  {
+    "revision": "be32c7bf30fc8e497c25",
+    "url": "/dictionary/static/js/runtime~main.be32c7bf.js"
+  },
+  {
+    "revision": "2994af714ca49e58adfc5ff984d94ba9",
+    "url": "/dictionary/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dictionary/precache-manifest.9dfc706b7ecbbae080ac8976f6a52ffb.js"
+  "/dictionary/precache-manifest.e8625aa25acfa0a8b95cb8c96b10804e.js"
 );
 
 workbox.clientsClaim();
